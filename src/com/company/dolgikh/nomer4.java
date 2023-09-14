36d20ccfc35de61b4be825deee76c9af6558926b
Katya <katyadolgikh2003@gmail.com>
2023-09-14 12:09:32 +0300

    test commit

---
+++ src/com/company/dolgikh/nomer4.java
@@ -0,0 +1,9 @@
+package com.company.dolgikh;
+
+public class nomer4 {
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            System.out.println("Аргумент " + (i + 1) + ": " + args[i]);
+        }
+    }
+}
